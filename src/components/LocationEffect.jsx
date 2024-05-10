9450f98e7252018afcef450f19c43ea44b5a595a
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-05-10 15:00:51 +0200

    Test of Github.
    Informacion about Hooks is completed.

--- src/components/LocationEffect.jsx
+++ src/components/LocationEffect.jsx
@@ -7,20 +7,24 @@ const LocationEffect = () => {
   const nameRegex = /^[a-zA-Z\s]*$/;
 
   useEffect(() => {
-   if (nameRegex.test(name) || name.length == 0) {
-    setMessage(" ");
-   } else {
-    document.getElementById("validity").style.color = "#FC4455";
-    setMessage("Insert only letters");
-   }
-    
-  }, [name])
-  
+    if (nameRegex.test(name) || name.length == 0) {
+      setMessage(" ");
+    } else {
+      document.getElementById("validity").style.color = "#FC4455";
+      setMessage("Insert only letters");
+    }
+  }, [name]);
 
   return (
     <div className="w-scree h-screen bg-[#626262] flex flex-col justify-center items-center">
-        <label className="text-[#EEEEEE] mb-2" htmlFor="text">Insert your name:</label>
-      <input className="mt-1 rounded-lg" onChange={(event) => setName(event.target.value)} type="text" />
+      <label className="text-[#EEEEEE] mb-2" htmlFor="text">
+        Insert your name:
+      </label>
+      <input
+        className="mt-1 rounded-lg"
+        onChange={(event) => setName(event.target.value)}
+        type="text"
+      />
       <br />
       <div id="validity">{message}</div>
     </div>
